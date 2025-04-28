577c92442221ad8a8ebef5533b1958d6afa06e30
Donald Batryn <donald.batryn@gmail.com>
2025-04-27 20:58:32 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,124 +1,350 @@
-jQuery(document).ready(function($) {
+/* ===================================================================
+ * Luther 1.0.0 - Main JS
+ *
+ * ------------------------------------------------------------------- */
 
-	'use strict';
+(function (html) {
+  "use strict";
 
-    $('.imageGallery1 a').simpleLightbox();
+  html.className = html.className.replace(/\bno-js\b/g, "") + " js ";
 
-
-    var owl = $("#owl-portfolio");
-
-      owl.owlCarousel({
-        
-        pagination : true,
-        paginationNumbers: false,
-        autoPlay: 6000, //Set AutoPlay to 3 seconds
-        items : 4, //10 items above 1000px browser width
-        itemsDesktop : [1000,4], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,3], // betweem 900px and 601px
-        itemsTablet: [600,2], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-        
-    });
-
-
-    $('.tabgroup > div').hide();
-    $('.tabgroup > div:first-of-type').show();
-    $('.tabs a').click(function(e){
-      e.preventDefault();
-        var $this = $(this),
-        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-        others = $this.closest('li').siblings().children('a'),
-        target = $this.attr('href');
-    others.removeClass('active');
-    $this.addClass('active');
-    $(tabgroup).children('div').hide();
-    $(target).show();
-  
+  /* Animations
+   * -------------------------------------------------- */
+  const tl = anime
+    .timeline({
+      easing: "easeInOutCubic",
+      duration: 800,
+      autoplay: false,
     })
+    .add({
+      targets: "#loader",
+      opacity: 0,
+      duration: 1000,
+      begin: function (anim) {
+        window.scrollTo(0, 0);
+      },
+    })
+    .add({
+      targets: "#preloader",
+      opacity: 0,
+      complete: function (anim) {
+        document.querySelector("#preloader").style.visibility = "hidden";
+        document.querySelector("#preloader").style.display = "none";
+      },
+    })
+    .add(
+      {
+        targets: ".s-header",
+        translateY: [-100, 0],
+        opacity: [0, 1],
+      },
+      "-=200"
+    )
+    // .add(
+    //   {
+    //     targets: ".s-intro-img",
+    //     translateY: [-100, 0],
+    //     opacity: [0, 1],
+    //   },
+    //   "-=600"
+    // )
+    .add({
+      targets: [".s-intro .text-pretitle", ".s-intro .text-huge-title"],
+      translateX: [100, 0],
+      opacity: [0, 1],
+      delay: anime.stagger(400),
+    })
+    .add({
+      targets: ".circles span",
+      keyframes: [
+        { opacity: [0, 0.3] },
+        {
+          opacity: [0.3, 0.1],
+          delay: anime.stagger(100, { direction: "reverse" }),
+        },
+      ],
+      delay: anime.stagger(100, { direction: "reverse" }),
+    })
+    .add({
+      targets: ".intro-social li",
+      translateX: [-50, 0],
+      opacity: [0, 1],
+      delay: anime.stagger(100, { direction: "reverse" }),
+    })
+    .add(
+      {
+        targets: ".intro-scrolldown",
+        translateY: [100, 0],
+        opacity: [0, 1],
+      },
+      "-=800"
+    );
 
+  /* Preloader
+   * -------------------------------------------------- */
+  const ssPreloader = function () {
+    const preloader = document.querySelector("#preloader");
+    if (!preloader) return;
 
-	/************** Toggle *********************/
-    // Cache selectors
-    var lastId,
-        topMenu = $(".menu-first"),
-        topMenuHeight = 80,
-        // All list items
-        menuItems = topMenu.find("a"),
-        // Anchors corresponding to menu items
-        scrollItems = menuItems.map(function(){
-          
-          if($(this).hasClass('external')) {
-            return;
-          }
-            
-          var item = $($(this).attr("href"));
-          if (item.length) { return item; }
-        });
+    window.addEventListener("load", function () {
+      document.querySelector("html").classList.remove("ss-preload");
+      document.querySelector("html").classList.add("ss-loaded");
 
-    // Bind click handler to menu items
-    // so we can get a fancy scroll animation
-    menuItems.click(function(e){
-      var href = $(this).attr("href"),
-          offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-      $('html, body').stop().animate({ 
-          scrollTop: offsetTop
-      }, 750);
-      e.preventDefault();
+      document.querySelectorAll(".ss-animated").forEach(function (item) {
+        item.classList.remove("ss-animated");
+      });
+
+      tl.play();
     });
 
-    // Bind to scroll
-    $(window).scroll(function(){
-       // Get container scroll position
-       var fromTop = $(this).scrollTop()+topMenuHeight;
-       
-       // Get id of current scroll item
-       var cur = scrollItems.map(function(){
-         if ($(this).offset().top < fromTop)
-           return this;
-       });
-       // Get the id of the current element
-       cur = cur[cur.length-1];
-       var id = cur && cur.length ? cur[0].id : "";
-       
-       if (lastId !== id) {
-           lastId = id;
-           // Set/remove active class
-           menuItems
-             .parent().removeClass("active")
-             .end().filter("[href=#"+id+"]").parent().addClass("active");
-       }                   
+    // force page scroll position to top at page refresh
+    // window.addEventListener('beforeunload' , function () {
+    //     // window.scrollTo(0, 0);
+    // });
+  }; // end ssPreloader
+
+  /* Mobile Menu
+   * ---------------------------------------------------- */
+  const ssMobileMenu = function () {
+    const toggleButton = document.querySelector(".mobile-menu-toggle");
+    const mainNavWrap = document.querySelector(".main-nav-wrap");
+    const siteBody = document.querySelector("body");
+
+    if (!(toggleButton && mainNavWrap)) return;
+
+    toggleButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      toggleButton.classList.toggle("is-clicked");
+      siteBody.classList.toggle("menu-is-open");
     });
 
-
-
-    $(window).scroll(function(){
-         $('.main-header').toggleClass('scrolled', $(this).scrollTop() > 1);
-     });
-
-
-
-    $('a[href="#top"]').click(function(){
-        $('html, body').animate({scrollTop: 0}, 'slow');
-        return false;
+    mainNavWrap.querySelectorAll(".main-nav a").forEach(function (link) {
+      link.addEventListener("click", function (event) {
+        // at 800px and below
+        if (window.matchMedia("(max-width: 800px)").matches) {
+          toggleButton.classList.toggle("is-clicked");
+          siteBody.classList.toggle("menu-is-open");
+        }
+      });
     });
 
+    window.addEventListener("resize", function () {
+      // above 800px
+      if (window.matchMedia("(min-width: 801px)").matches) {
+        if (siteBody.classList.contains("menu-is-open"))
+          siteBody.classList.remove("menu-is-open");
+        if (toggleButton.classList.contains("is-clicked"))
+          toggleButton.classList.remove("is-clicked");
+      }
+    });
+  }; // end ssMobileMenu
 
-    $('.toggle-menu').click(function(){
-        $('.menu-first').toggleClass('show');
-        // $('.menu-first').slideToggle();
+  /* Highlight active menu link on pagescroll
+   * ------------------------------------------------------ */
+  const ssScrollSpy = function () {
+    const sections = document.querySelectorAll(".target-section");
+
+    // Add an event listener listening for scroll
+    window.addEventListener("scroll", navHighlight);
+
+    function navHighlight() {
+      // Get current scroll position
+      let scrollY = window.pageYOffset;
+
+      // Loop through sections to get height(including padding and border),
+      // top and ID values for each
+      sections.forEach(function (current) {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+
+        /* If our current scroll position enters the space where current section
+         * on screen is, add .current class to parent element(li) of the thecorresponding
+         * navigation link, else remove it. To know which link is active, we use
+         * sectionId variable we are getting while looping through sections as
+         * an selector
+         */
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          document
+            .querySelector(".main-nav a[href*=" + sectionId + "]")
+            .parentNode.classList.add("current");
+        } else {
+          document
+            .querySelector(".main-nav a[href*=" + sectionId + "]")
+            .parentNode.classList.remove("current");
+        }
+      });
+    }
+  }; // end ssScrollSpy
+
+  /* Animate elements if in viewport
+   * ------------------------------------------------------ */
+  const ssViewAnimate = function () {
+    const blocks = document.querySelectorAll("[data-animate-block]");
+
+    window.addEventListener("scroll", viewportAnimation);
+
+    function viewportAnimation() {
+      let scrollY = window.pageYOffset;
+
+      blocks.forEach(function (current) {
+        console.log({ current });
+        const viewportHeight = window.innerHeight;
+        const triggerTop =
+          current.offsetTop + viewportHeight * 0.2 - viewportHeight;
+        const blockHeight = current.offsetHeight;
+        const blockSpace = triggerTop + blockHeight;
+        const inView = scrollY > triggerTop && scrollY <= blockSpace;
+        const isAnimated = current.classList.contains("ss-animated");
+        console.log("is inView: ", inView);
+        if (inView && !isAnimated) {
+          anime({
+            targets: current.querySelectorAll("[data-animate-el]"),
+            opacity: [0, 1],
+            translateY: [100, 0],
+            delay: anime.stagger(400, { start: 200 }),
+            duration: 800,
+            easing: "easeInOutCubic",
+            begin: function (anim) {
+              current.classList.add("ss-animated");
+            },
+          });
+        }
+      });
+    }
+  }; // end ssViewAnimate
+
+  /* Swiper
+   * ------------------------------------------------------ */
+  const ssSwiper = function () {
+    const mySwiper = new Swiper(".swiper-container", {
+      slidesPerView: 1,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        // when window width is > 400px
+        401: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        // when window width is > 800px
+        801: {
+          slidesPerView: 2,
+          spaceBetween: 32,
+        },
+        // when window width is > 1200px
+        1201: {
+          slidesPerView: 2,
+          spaceBetween: 80,
+        },
+      },
+    });
+  }; // end ssSwiper
+
+  /* Lightbox
+   * ------------------------------------------------------ */
+  const ssLightbox = function () {
+    const folioLinks = document.querySelectorAll(".folio-list__item-link");
+    const modals = [];
+
+    folioLinks.forEach(function (link) {
+      let modalbox = link.getAttribute("href");
+      let instance = basicLightbox.create(document.querySelector(modalbox), {
+        onShow: function (instance) {
+          //detect Escape key press
+          document.addEventListener("keydown", function (event) {
+            event = event || window.event;
+            if (event.keyCode === 27) {
+              instance.close();
+            }
+          });
+        },
+      });
+      modals.push(instance);
     });
 
-    $('.menu-first li a').click(function(){
-      $('.menu-first').removeClass('show');
+    folioLinks.forEach(function (link, index) {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        modals[index].show();
+      });
     });
+  }; // end ssLightbox
 
+  /* Alert boxes
+   * ------------------------------------------------------ */
+  const ssAlertBoxes = function () {
+    const boxes = document.querySelectorAll(".alert-box");
 
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top -79 }, 500, 'linear');
+    boxes.forEach(function (box) {
+      box.addEventListener("click", function (event) {
+        if (event.target.matches(".alert-box__close")) {
+          event.stopPropagation();
+          event.target.parentElement.classList.add("hideit");
+
+          setTimeout(function () {
+            box.style.display = "none";
+          }, 500);
+        }
+      });
     });
+  }; // end ssAlertBoxes
 
+  /* Smoothscroll
+   * ------------------------------------------------------ */
+  const ssMoveTo = function () {
+    const easeFunctions = {
+      easeInQuad: function (t, b, c, d) {
+        t /= d;
+        return c * t * t + b;
+      },
+      easeOutQuad: function (t, b, c, d) {
+        t /= d;
+        return -c * t * (t - 2) + b;
+      },
+      easeInOutQuad: function (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t + b;
+        t--;
+        return (-c / 2) * (t * (t - 2) - 1) + b;
+      },
+      easeInOutCubic: function (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t * t + b;
+        t -= 2;
+        return (c / 2) * (t * t * t + 2) + b;
+      },
+    };
 
+    const triggers = document.querySelectorAll(".smoothscroll");
 
+    const moveTo = new MoveTo(
+      {
+        tolerance: 0,
+        duration: 1200,
+        easing: "easeInOutCubic",
+        container: window,
+      },
+      easeFunctions
+    );
 
-});
+    triggers.forEach(function (trigger) {
+      moveTo.registerTrigger(trigger);
+    });
+  }; // end ssMoveTo
+
+  /* Initialize
+   * ------------------------------------------------------ */
+  (function ssInit() {
+    ssPreloader();
+    ssMobileMenu();
+    ssScrollSpy();
+    ssViewAnimate();
+    ssSwiper();
+    ssLightbox();
+    ssAlertBoxes();
+    ssMoveTo();
+  })();
+})(document.documentElement);
